c8f3b01bf2a3a7c4975b63f075ac06f2ac248d8d
gcronax <146924808+gcronax@users.noreply.github.com>
2025-03-03 18:41:12 +0100

    sads
    asdasd

---
+++ main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+
+        int aux;
+        do {
+            System.out.println("dime que deseas hacer: " +
+                    "\n 1 Gestion pisos" +
+                    "\n 2 Gestion propietarios" +
+                    "\n 3 Gestion inquilinos" +
+                    "\n 0 Finalizar programa");
+
+            aux=scan.nextInt();
+
+            switch (aux){
+                case 1 -> tablas.menuTablas("piso","pisos");
+                case 2 -> tablas.menuTablas("propietario","propietarios");
+                case 3 -> tablas.menuTablas("inquilino","inquilinos");
+                case 4 -> tablas.menuTablas("poblacio","poblacions");
+                case 5 -> tablas.menuTablas("institut","instituts");
+
+
+            }
+        }while (aux!=0);
+    }
+}
